5ed0c92d2016f62d736c6691b9237bc4903fc83d
LorenzoRuzza <lorenzo.ruzza@studenti.unipd.it>
2021-10-09 16:24:51 +0200

    ripristinata situazione di partenza riveduta e corretta

---
+++ fileArduino/build/CMakeFiles/DrawBot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for DrawBot.
+# This may be replaced when dependencies are built.
---
+++ fileArduino/build/lib/Servo/CMakeFiles/Servo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/the_lorlito/Documenti/DrawBot_CNC/fileArduino/lib/Servo/src/Servo.cpp" "lib/Servo/CMakeFiles/Servo.dir/src/Servo.cpp.o" "gcc" "lib/Servo/CMakeFiles/Servo.dir/src/Servo.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/the_lorlito/Documenti/DrawBot_CNC/fileArduino/build/CMakeFiles/ArduinoCore.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
